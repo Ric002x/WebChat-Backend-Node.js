d9653da42b8134bc09ce1b9835073fe60c533ca9
Ricardo Oliveira <ricvenicius@gmail.com>
2026-01-16 15:22:56 -0300

    stating project

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express, { type Request, type Response } from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Configuring Api Server
+const app = express()
+const PORT = 5000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use('/static', express.static(path.join(__dirname, 'static')))
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on Port: ${PORT}`)
+})
+
+app.get('/', (req: Request, res: Response) => {
+    res.send({ message: "Hello, i'm using typescript for this project" })
+});
